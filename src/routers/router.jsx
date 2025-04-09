dc8acdc8736e4e073b9ee2e9ccd097f4ab2af9e7
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-04-09 08:43:10 +0430

    Add Update Book, Update router

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -14,6 +14,7 @@ import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
 import AddBook from "../pages/dashboard/addBook/AddBook";
+import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
 
 const router = createBrowserRouter([
   {
@@ -95,7 +96,7 @@ const router = createBrowserRouter([
         path: "edit-book/:id",
         element: (
           <AdminRoute>
-            <div>Edit Book </div>
+            <UpdateBook />
           </AdminRoute>
         ),
       },
